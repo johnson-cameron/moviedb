3f3991f8103e8319e81fa1561a8d435daf28d8d8
unknown <kevin.williams49531@gmail.com>
2018-05-31 09:03:45 -0400

    Getting IDs

--- JS/Components/searchcomp.js
+++ JS/Components/searchcomp.js
@@ -2,16 +2,30 @@
 
 const search = {
   template:`
-  <form ng-submit="$ctrl.searchForm($ctrl.movie)">
+  <form ng-submit="$ctrl.searchFormTitle($ctrl.movie)">
     <input type="text" ng-model="$ctrl.movie"></input>
     <button>Find Movies</button>
   </form>
+  
+
+  <form ng-submit="$ctrl.searchFormGenre($ctrl.genre)">
+    <input type="text" ng-model="$ctrl.genre"></input>
+    <button>Find Genre</button>
+  </form>
   <movie-list response="$ctrl.response"></movie-list>
   `,
+  
   controller: ["MovieService", function( MovieService) {
     const vm = this;
-    vm.searchForm = (movie) => {
-      MovieService.search(movie).then((response) => {
+    vm.searchFormTitle = (movie) => {
+      MovieService.searchTitle(movie).then((response) => {
+        vm.response = response.data.results;
+        console.log(vm.response);
+      });
+    };
+
+    vm.searchFormGenre = (genre) => {
+      MovieService.searchGenre(genre).then((response) => {
         vm.response = response.data.results;
         console.log(vm.response);
       });
--- JS/service.js
+++ JS/service.js
@@ -19,7 +19,7 @@ function MovieService($http, $location) {
   const removeFav = () => {
     console.log("removeFav");
   };
-  const search = (movie) => {
+  const searchTitle = (movie) => {
     return $http({
       method: "GET",
       url: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movie}`
@@ -28,18 +28,48 @@ function MovieService($http, $location) {
       console.log(response);
       return response;
     });
-
-
     console.log("search");
     console.log(movie);
   }
+  const searchGenre = (genre) => {
+    return $http({
+      method: "GET",
+      url: `https://api.themoviedb.org/3/genre/movie/list?api_key=f74deb744f18c80a55023593e6d85143&language=en-US`,     
+      // url: `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&discover/movie?with_genres=${response.data.genres[index].id}`
+      // url: `https://api.themoviedb.org/3/discover/movie?api_key=f74deb744f18c80a55023593e6d85143&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+    }).then((response) => {
+      favList = response
+      // console.log(response.data.genres);
+
+      let upGenre = genre.charAt(0).toUpperCase() + genre.substr(1);
+      // console.log(upGenre);
+
+      for (let index = 0; index < response.data.genres.length; ++index) {
+        // console.log(genre);
+        
+        // console.log(response.data.genres[index].name);
+        if (upGenre == response.data.genres[index].name) {
+          console.log(`The genre id is ${response.data.genres[index].id}`)
+        };
+    }
+      return response;
+    });
+    console.log("search");
+    console.log(genre);
+  }
+
+
+
+
+
   return {
     getFavList,
     toSearch,
     movieInfo,
     addFav,
     removeFav,
-    search
+    searchTitle,
+    searchGenre
   };
 }
 
